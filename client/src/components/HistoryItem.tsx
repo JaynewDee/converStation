a03c21762529c5c3c6a81032554b5e814f632388
NuminousBlue <jdiehl2236@gmail.com>
2022-05-22 20:55:49 -0500

    fixing issue with invisible submit button on ios

--- client/src/components/HistoryItem.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HistoryItem: React.FC = () => {
-  return (
-    <div>I am a SINGLE ITEM in the list of fetch histories</div>
-  )
-}
-
-export default HistoryItem;
